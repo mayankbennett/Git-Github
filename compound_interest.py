bdd0501e23129d591c520214f03b116eb795c2d8
mayankbennett <142829308+mayankbennett@users.noreply.github.com>
2024-03-31 19:57:12 +0530

    Create compound_interest.py

---
+++ compound_interest.py
@@ -0,0 +1,10 @@
+def compound_interest(p, t, r):
+    return p * (pow((1 + r / 100), t))
+
+
+if __name__ == "__main__":
+    p = float(input("Enter the principal amount: "))
+    t = float(input("Enter the time period: "))
+    r = float(input("Enter the rate of interest: "))
+
+    print("The compound interest is {:.2f}".format(compound_interest(p, t, r)))
